2947ab6d0776e76e2a37eb631c9c19e842a85452
KeshavGarg144 <49794557+KeshavGarg144@users.noreply.github.com>
2020-10-15 21:50:58 +0530

    Update revrse.java

--- revrse.java
+++ revrse.java
@@ -1,18 +1,18 @@
-import java.util.*;
+import java.io.*;
 class revrse
 {
-public static void main(String s[])
+public static void main(String args[])throws IOException
 {
 int num,rev=0,r;
-Scanner sc=new Scanner(System.in);
+BUfferedReader br=new InputStreamReader(System.in()) ;
 System.out.print("Enter a number= ");
-num=sc.nextInt();
+num=integer.parseInt(br.readLine());
 while(num>0)
 {
 	r=num%10;
 	rev=(rev*10)+r;
 	num=num/10;
 }
-System.out.print("Revrse is= "+rev);
+System.out.println("Reverse is= "+rev);
 }
 }
